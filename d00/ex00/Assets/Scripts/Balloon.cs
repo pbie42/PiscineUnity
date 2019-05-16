442243506bca324bf5df26bb00d6c6a6385a0008
Paul BIE <pbie@e1r7p6.42.fr>
2019-05-16 16:26:57 +0200

    [Wip] - d00 ex00

---
+++ d00/ex00/Assets/Scripts/Balloon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("Space Bar Pressed");
+			gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+			Debug.Log(gameObject.transform.localScale);
+			if (gameObject.transform.localScale.x >= 4.0)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
