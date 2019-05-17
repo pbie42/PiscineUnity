5c3b96b6f5a9b5935ebd4f73233763634e02e574
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-17 16:01:34 +0200

    [Add] - Game Over for too small, too long without action, and too big

--- d00/ex00/Assets/Scripts/Balloon.cs
+++ d00/ex00/Assets/Scripts/Balloon.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class Balloon : MonoBehaviour
 {
 
+	private bool _gameStarted = false;
+	private float _gameOverTimer = 0.0f;
+	private float _timer = 0.0f;
+	private Vector3 _localScale;
+	private Vector3 _gameOverScale = new Vector3(0f, 0f, 0f);
+	private Vector3 _explodeScale = new Vector3(4.0f, 4.0f, 0f);
+	[SerializeField] float _descale = 0.0f;
+
 	// Use this for initialization
 	void Start()
 	{
+		_localScale = gameObject.transform.localScale;
 		gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		Vector3 currentScale = gameObject.transform.localScale;
+
+		if (_gameStarted)
+		{
+			_timer += Time.deltaTime;
+			_gameOverTimer += Time.deltaTime;
+			gameObject.transform.localScale -= new Vector3(_descale, _descale, 0);
+		}
+		Debug.Log("currentScale " + currentScale.ToString("F4"));
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			Debug.Log("Space Bar Pressed");
-			gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0);
-			Debug.Log(gameObject.transform.localScale);
+			_gameStarted = true;
+			_gameOverTimer = 0.0f;
+			gameObject.transform.localScale += new Vector3(0.05f, 0.05f, 0);
 			if (gameObject.transform.localScale.x >= 4.0)
-			{
 				Destroy(gameObject);
-			}
+		}
+		if (_gameOverTimer >= 5f || currentScale.x <= 0.0f || currentScale.x >= 4.0f)
+		{
+			Debug.Log("Game Over");
+			Destroy(gameObject);
 		}
 	}
 }
