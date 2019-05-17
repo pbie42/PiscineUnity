bc2c8aa926a17107de44f0acbf2efff089462c66
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-17 17:35:31 +0200

    [Wip] - Cooldown for breathing

--- d00/ex00/Assets/Scripts/Balloon.cs
+++ d00/ex00/Assets/Scripts/Balloon.cs
@@ -6,17 +6,18 @@ public class Balloon : MonoBehaviour
 {
 
 	private bool _gameStarted = false;
+	private float _descale = 0.001f;
+	private float _gameOverScaleMaxX = 4.0f;
+	private float _gameOverScaleMinX = 0.0f;
+	private float _gameOverTime = 5.0f;
 	private float _gameOverTimer = 0.0f;
 	private float _timer = 0.0f;
-	private Vector3 _localScale;
-	private Vector3 _gameOverScale = new Vector3(0f, 0f, 0f);
-	private Vector3 _explodeScale = new Vector3(4.0f, 4.0f, 0f);
-	[SerializeField] float _descale = 0.0f;
+	private int _breaths = 0;
+	private float _coolDown = 0.0f;
 
 	// Use this for initialization
 	void Start()
 	{
-		_localScale = gameObject.transform.localScale;
 		gameObject.transform.localScale = new Vector3(0.2f, 0.2f, 0);
 	}
 
@@ -24,26 +25,38 @@ public class Balloon : MonoBehaviour
 	void Update()
 	{
 		Vector3 currentScale = gameObject.transform.localScale;
-
 		if (_gameStarted)
-		{
-			_timer += Time.deltaTime;
-			_gameOverTimer += Time.deltaTime;
-			gameObject.transform.localScale -= new Vector3(_descale, _descale, 0);
-		}
-		Debug.Log("currentScale " + currentScale.ToString("F4"));
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			_gameStarted = true;
-			_gameOverTimer = 0.0f;
-			gameObject.transform.localScale += new Vector3(0.05f, 0.05f, 0);
-			if (gameObject.transform.localScale.x >= 4.0)
-				Destroy(gameObject);
-		}
-		if (_gameOverTimer >= 5f || currentScale.x <= 0.0f || currentScale.x >= 4.0f)
-		{
-			Debug.Log("Game Over");
-			Destroy(gameObject);
-		}
+			GameFrame();
+		if (Input.GetKeyDown(KeyCode.Space) && _breaths <= 5)
+			SpaceBarPressed();
+		if (_gameOverTimer >= _gameOverTime
+			|| currentScale.x <= _gameOverScaleMinX
+			|| currentScale.x >= _gameOverScaleMaxX
+			)
+			GameOver();
+	}
+
+	private void GameFrame()
+	{
+		_timer += Time.deltaTime;
+		_gameOverTimer += Time.deltaTime;
+		gameObject.transform.localScale -= new Vector3(_descale, _descale, 0);
+	}
+
+	private void SpaceBarPressed()
+	{
+		_breaths++;
+		if (_breaths > 5)
+			_coolDown = 0.5f;
+		_gameStarted = true;
+		_gameOverTimer = 0.0f;
+		gameObject.transform.localScale += new Vector3(0.05f, 0.05f, 0);
+	}
+
+	private void GameOver()
+	{
+		Debug.Log("Game Over");
+		Debug.Log("Balloon life time: " + Mathf.RoundToInt(_timer) + 's');
+		Destroy(gameObject);
 	}
 }
