03701c1010209d2ce623d33c20a09faa29724d64
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-18 20:10:19 +0200

    [Add] - d00 ex01 beginnings

---
+++ d00/ex01/Assets/Scripts/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+	private Vector2 _screenBounds;
+	private Vector3 _speed;
+	private float _maxSpeed = 0.2f;
+	private float _minSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start()
+	{
+		_screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+		_speed = new Vector3(0.0f, Random.Range(_minSpeed, _maxSpeed));
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position -= _speed;
+		if (transform.position.y < -15)
+			Destroy(gameObject);
+	}
+}
---
+++ d00/ex01/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+	public GameObject[] spawnees;
+	private string[] keys = { "a(Clone)", "s(Clone)", "d(Clone)" };
+	private bool _gameStarted = false;
+	private float _spawnTimer = 0.6f;
+
+	private void Start()
+	{
+		SpawnCube();
+	}
+
+	private void SpawnCube()
+	{
+		bool found = true;
+		int chosen = 0;
+		while (found)
+		{
+			Debug.Log("We in here");
+			chosen = Random.Range(0, spawnees.Length);
+			GameObject foundObject = GameObject.Find(keys[chosen]);
+			if (foundObject)
+				Debug.Log("name: " + foundObject.name);
+			if (!foundObject)
+				found = false;
+		}
+		GameObject cube = Instantiate(spawnees[chosen]) as GameObject;
+		Debug.Log("Cube Name " + cube.name);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_spawnTimer -= Time.deltaTime;
+
+		if (_spawnTimer <= 0 && NeedCube())
+		{
+			_spawnTimer = 0.6f;
+			SpawnCube();
+		}
+	}
+
+	private bool NeedCube()
+	{
+		GameObject a = GameObject.Find(keys[0]);
+		GameObject s = GameObject.Find(keys[1]);
+		GameObject d = GameObject.Find(keys[2]);
+		if (!a || !s || !d)
+			return true;
+		return false;
+	}
+}
