4681578f8f99c1471e9baefb2c1844ef7fa6df79
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-19 23:48:11 +0200

    [Add] - d00 ex04 - Pong

--- d00/Assets/ex04/Scripts/Player.cs
+++ d00/Assets/ex04/Scripts/Player.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
+	private float _playerMax = 4.5f;
+	private float _diffX = 0.2f;
+	private float _diffY = 1f;
+	private int _score = 0;
+	private Vector3 _playerSpeed = new Vector3(0, 0.3f, 0);
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
+	}
+
+	public void MoveUp()
+	{
+		if (gameObject.transform.position.y < _playerMax)
+			gameObject.transform.position += _playerSpeed;
+	}
+
+	public void MoveDown()
+	{
+		if (gameObject.transform.position.y > -_playerMax)
+			gameObject.transform.position -= _playerSpeed;
+	}
+
+	public void AddPoint()
+	{
+		_score++;
+	}
+
+	public int GetScore()
+	{
+		return _score;
+	}
+
+	public bool CheckCollision(Vector3 ball)
+	{
+		float paddleY = gameObject.transform.position.y;
+		float paddleX = gameObject.transform.position.x;
+		if (paddleX > 0)
+		{
+			if (ball.x > paddleX - _diffX && (ball.y >= paddleY - _diffY && ball.y <= paddleY + _diffY))
+				return true;
+		}
+		if (paddleX < 0)
+		{
+			if (ball.x < paddleX + _diffX && (ball.y >= paddleY - _diffY && ball.y <= paddleY + _diffY))
+				return true;
+		}
+		return false;
 	}
 }
--- d00/Assets/ex04/Scripts/PongBall.cs
+++ d00/Assets/ex04/Scripts/PongBall.cs
@@ -2,15 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PongBall : MonoBehaviour {
+public class PongBall : MonoBehaviour
+{
+	[SerializeField] Player[] players;
+	private Vector3 _ballStartPos;
+	private Vector3 _ballSpeedRight = new Vector3(0.1f, 0, 0);
+	private Vector3 _ballSpeedUp = new Vector3(0, 0.1f, 0);
+	private float _border = 5.3f;
+	private float _goal = 8f;
+	private bool _ballRight = true;
+	private bool _ballUp = true;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		_ballStartPos = gameObject.transform.position;
+		LaunchBall();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		MoveBall();
+		CheckCollisions();
+		if (Input.GetKey(KeyCode.DownArrow))
+			players[1].MoveDown();
+		if (Input.GetKey(KeyCode.UpArrow))
+			players[1].MoveUp();
+		if (Input.GetKey(KeyCode.S))
+			players[0].MoveDown();
+		if (Input.GetKey(KeyCode.W))
+			players[0].MoveUp();
+	}
+
+	private void LaunchBall()
+	{
+		gameObject.transform.position = _ballStartPos;
+		_ballRight = Random.Range(0, 2) == 1 ? true : false;
+		_ballUp = Random.Range(0, 2) == 1 ? true : false;
+	}
+
+	private void MoveBall()
+	{
+		if (_ballRight)
+			gameObject.transform.position += _ballSpeedRight;
+		else
+			gameObject.transform.position -= _ballSpeedRight;
+		if (_ballUp)
+			gameObject.transform.position += _ballSpeedUp;
+		else
+			gameObject.transform.position -= _ballSpeedUp;
+	}
+
+	private void CheckCollisions()
+	{
+		CheckBorderCollision();
+		CheckGoalCollision();
+		CheckPlayerCollision();
+	}
+
+	private void CheckBorderCollision()
+	{
+		float ballY = gameObject.transform.position.y;
+		float ballX = gameObject.transform.position.x;
+		if (ballY >= _border || ballY <= -_border)
+			_ballUp = !_ballUp;
+	}
+
+	private void CheckGoalCollision()
+	{
+		float ballY = gameObject.transform.position.y;
+		float ballX = gameObject.transform.position.x;
+		if (ballX >= _goal)
+		{
+			players[0].AddPoint();
+			PointScored();
+		}
+		else if (ballX <= -_goal)
+		{
+			players[1].AddPoint();
+			PointScored();
+		}
+	}
+
+	private void CheckPlayerCollision()
+	{
+		Vector3 ballPos = gameObject.transform.position;
+		if (players[0].CheckCollision(ballPos) || players[1].CheckCollision(ballPos))
+			_ballRight = !_ballRight;
+	}
+
+	private void PointScored()
+	{
+		Debug.Log("Player 1: " + players[0].GetScore() + " | Player 2: " + players[1].GetScore());
+		LaunchBall();
 	}
 }
