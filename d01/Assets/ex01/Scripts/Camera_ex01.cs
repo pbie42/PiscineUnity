2390a90c3b824879e0e212c2767580a0b0096009
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-20 19:02:17 +0200

    [Add] - d01 ex01 - Prefabs for player end locations. Game now has end when players reach their end locations

---
+++ d01/Assets/ex01/Scripts/Camera_ex01.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_ex01 : MonoBehaviour
+{
+
+	public playerScript_ex01 Thomas;
+	public playerScript_ex01 John;
+	public playerScript_ex01 Claire;
+	private playerScript_ex01 _currentCharacter;
+
+	// Use this for initialization
+	void Start()
+	{
+		_currentCharacter = Thomas;
+		_currentCharacter.EnterFocus();
+		gameObject.transform.position = _currentCharacter.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 newPosition = _currentCharacter.transform.position + new Vector3(0, 0, -10);
+		gameObject.transform.position = newPosition;
+		CheckChange();
+		CheckEnd();
+	}
+
+	private void CheckEnd()
+	{
+		if (Thomas.IsInEnd() && Claire.IsInEnd() && John.IsInEnd())
+			Debug.Log("You've Won!");
+	}
+
+	private void CheckChange()
+	{
+		if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
+			ChangeFocus(Thomas);
+		if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
+			ChangeFocus(John);
+		if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
+			ChangeFocus(Claire);
+		if (Input.GetKeyDown(KeyCode.Backspace))
+			ResetScene();
+	}
+
+	private void ResetScene()
+	{
+		Thomas.ResetPos();
+		John.ResetPos();
+		Claire.ResetPos();
+		ChangeFocus(Thomas);
+	}
+
+	private void ChangeFocus(playerScript_ex01 player)
+	{
+		_currentCharacter.LeaveFocus();
+		_currentCharacter = player;
+		_currentCharacter.EnterFocus();
+	}
+}
---
+++ d01/Assets/ex01/Scripts/playerScript_ex01.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript_ex01 : MonoBehaviour
+{
+
+	private bool _grounded = true;
+	private bool _inEndPos = false;
+	private bool _focused = false;
+	private float _lowJump = 400f;
+	private float _highJump = 550f;
+	private float _medJump = 450f;
+	private float _lowSpeed = 0.05f;
+	private float _medSpeed = 0.1f;
+	private float _highSpeed = 0.15f;
+	private float _endDiff = 0.1f;
+	private Rigidbody2D _rgbd;
+	private Vector3 _startPos;
+	public GameObject _end;
+
+	// Use this for initialization
+	void Start()
+	{
+		_startPos = gameObject.transform.position;
+		_rgbd = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (_focused)
+		{
+			Move();
+			if (Input.GetKeyDown(KeyCode.Space) && _grounded)
+				Jump();
+		}
+	}
+
+	private void Move()
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			if (gameObject.tag == "Claire")
+				MoveRightLeft(_lowSpeed, 1);
+			if (gameObject.tag == "Thomas")
+				MoveRightLeft(_medSpeed, 1);
+			if (gameObject.tag == "John")
+				MoveRightLeft(_highSpeed, 1);
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			if (gameObject.tag == "Claire")
+				MoveRightLeft(_lowSpeed, -1);
+			if (gameObject.tag == "Thomas")
+				MoveRightLeft(_medSpeed, -1);
+			if (gameObject.tag == "John")
+				MoveRightLeft(_highSpeed, -1);
+		}
+	}
+
+	private void MoveRightLeft(float speed, int direction)
+	{
+		gameObject.transform.position += (new Vector3((speed * direction), 0, 0));
+		CheckEnd();
+	}
+
+	private void CheckEnd()
+	{
+		float playerX = gameObject.transform.position.x;
+		float playerY = gameObject.transform.position.y;
+		float endX = gameObject.transform.position.x;
+		float endY = gameObject.transform.position.y;
+		if (playerX <= endX + _endDiff && playerX >= endX - _endDiff
+			&& playerY <= endY + _endDiff && playerY >= endY - _endDiff)
+			_inEndPos = true;
+		else
+			_inEndPos = false;
+	}
+
+	private void Jump()
+	{
+		if (gameObject.tag == "Claire")
+			_rgbd.AddForce(new Vector2(0, _lowJump));
+		else if (gameObject.tag == "Thomas")
+			_rgbd.AddForce(new Vector2(0, _medJump));
+		else if (gameObject.tag == "John")
+			_rgbd.AddForce(new Vector2(0, _highJump));
+		_grounded = false;
+	}
+
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		Debug.Log("Leaving");
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		string tag = other.gameObject.tag;
+		if (tag == "Bottom" || tag == "Thomas" || tag == "John" || tag == "Claire")
+			_grounded = true;
+	}
+
+	public void EnterFocus()
+	{
+		_focused = true;
+	}
+
+	public void LeaveFocus()
+	{
+		_focused = false;
+	}
+
+	public void IsGrounded(bool grounded)
+	{
+		_grounded = grounded;
+	}
+
+	public void ResetPos()
+	{
+		gameObject.transform.position = _startPos;
+	}
+
+	public bool IsInEnd()
+	{
+		return _inEndPos;
+	}
+}
