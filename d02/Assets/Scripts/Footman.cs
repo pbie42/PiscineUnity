f5c2ff145fe08913601da854d8181539a2d34242
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-22 17:38:47 +0200

    [Add] - d02 ex00 - Footman animation movement + highlighting for better selection clarity

--- d02/Assets/Scripts/Footman.cs
+++ d02/Assets/Scripts/Footman.cs
@@ -10,34 +10,40 @@ public class Footman : MonoBehaviour
 	private float _speed = 1.2f;
 	private bool _move = false;
 	private Vector3 _destination = Vector3.zero;
+	private SpriteRenderer _sprite;
+	private string _currentDir = "South";
 
 	public AudioClip[] selectedSounds;
 	public AudioClip[] orderedSounds;
+	public Animator animator;
 
 	// Use this for initialization
 	void Start()
 	{
+		_sprite = GetComponent<SpriteRenderer>();
 		_audioSources = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (_selected)
+			_sprite.color = hexColor(135.0f, 255.0f, 0f, 255f);
+		else
+			_sprite.color = hexColor(255.0f, 255.0f, 255.0f, 255f);
 		if (_move)
 		{
 			Move();
 		}
+
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 	}
 
-	// private void OnMouseDown()
-	// {
-	// 	_audioSources[0].Play();
-	// 	_selected = true;
-	// }
-
-	// private void OnMouseUp()
-	// {
-	// }
+	public Vector4 hexColor(float r, float g, float b, float a)
+	{
+		Vector4 color = new Vector4(r / 255, g / 255, b / 255, a / 255);
+		return color;
+	}
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
@@ -68,21 +74,82 @@ public class Footman : MonoBehaviour
 	public void MoveOrdered(Vector3 destination)
 	{
 		OrderedSound();
-		_destination = destination;
+		_destination = new Vector3(destination.x, destination.y, transform.position.z);
 		_movement = _destination - transform.position;
 		_movement = _movement.normalized;
 		_move = true;
+		animator.SetBool(_currentDir, false);
+	}
+
+	private void CalculateAnimation()
+	{
+		float degree = Vector3.Angle(Vector3.up, _destination);
+		if (_destination.x < transform.position.x)
+			degree = 180 + 180 - degree;
+		Debug.Log("degree " + degree);
+		_sprite.flipX = false;
+		if (degree >= 337.5 && degree <= 22.5)
+		{
+			Debug.Log("North");
+			_currentDir = "North";
+			_sprite.flipX = false;
+		}
+		else if (degree >= 22.5 && degree <= 67.5)
+		{
+			_currentDir = "NorthEast";
+			_sprite.flipX = false;
+		}
+		else if (degree >= 67.5 && degree <= 112.5)
+		{
+			_currentDir = "East";
+			_sprite.flipX = false;
+		}
+		else if (degree >= 112.5 && degree <= 157.5)
+		{
+			_currentDir = "SouthEast";
+			_sprite.flipX = false;
+		}
+		else if (degree >= 157.5 && degree <= 202.5)
+		{
+			_currentDir = "South";
+			_sprite.flipX = false;
+		}
+		else if (degree >= 202.5 && degree <= 247.5)
+		{
+			_currentDir = "SouthEast";
+			_sprite.flipX = true;
+		}
+		else if (degree >= 247.5 && degree <= 292.5)
+		{
+			_currentDir = "East";
+			_sprite.flipX = true;
+		}
+		else if (degree >= 292.5 && degree <= 337.5)
+		{
+			_currentDir = "NorthEast";
+			_sprite.flipX = true;
+		}
+		animator.SetBool(_currentDir, true);
+	}
+
+	public void GetAngle()
+	{
+
 	}
 
 	public void Move()
 	{
+		CalculateAnimation();
 		transform.Translate(_speed * _movement * Time.deltaTime);
 		float destX = _destination.x;
 		float destY = _destination.y;
 		float posX = transform.position.x;
 		float posY = transform.position.y;
-		if (posX == destX && posY == destY)
+		if (Vector3.Distance(_destination, transform.position) < 0.1f)
+		{
 			_move = false;
+			animator.SetBool(_currentDir, false);
+		}
 	}
 
 	private void SelectSound()
@@ -98,4 +165,5 @@ public class Footman : MonoBehaviour
 		int selectedSound = Random.Range(0, orderedSounds.Length - 1);
 		_audioSources.PlayOneShot(orderedSounds[selectedSound]);
 	}
+
 }
