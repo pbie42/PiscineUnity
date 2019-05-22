25ebd68c6bf96cda97da5bd16f349c7272f2a027
Paul BIE <pbie@e1r8p5.42.fr>
2019-05-22 11:55:15 +0200

    [Wip] - d02 ex00 - Moving footman

--- d02/Assets/Scripts/Footman.cs
+++ d02/Assets/Scripts/Footman.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Footman : MonoBehaviour {
+public class Footman : MonoBehaviour
+{
+	private AudioSource[] _audioSources;
+	private bool _selected = false;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		_audioSources = GetComponents<AudioSource>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
 	}
+
+	private void OnMouseDown()
+	{
+		Debug.Log("Mouse down on Footman");
+		_audioSources[0].Play();
+		_selected = true;
+	}
+
+	private void OnMouseUp()
+	{
+		Debug.Log("Mouse up on Footman");
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log("Collision");
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Trigger");
+	}
+
+	public void Deselect()
+	{
+		_selected = false;
+	}
+
+
 }
---
+++ d02/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+	private List<Footman> _footmen;
+
+	// Use this for initialization
+	void Start()
+	{
+		_footmen = new List<Footman>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// if (Input.GetMouseButtonDown(0))
+		// {
+		// 	Debug.Log("Input.mousePosition" + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		// }
+	}
+}
