90bd083a6542beb632729f3d6afeb80c8e96969f
Paul BIE <pbie@e1r7p6.42.fr>
2019-05-29 22:16:12 +0200

    [Add] - Smoke to block cameras

--- d06/Assets/Scripts/FPSController.cs
+++ d06/Assets/Scripts/FPSController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
-	public float speed = 5.0f;
-	public float sensitivity = 5.0f;
-	public UnityEngine.UI.Image detectionBar;
-	private float _detection = 0f;
-	private bool _detectedLight = false;
-	private float _detectionTotal = 200f;
-
 	CharacterController player;
-	public GameObject camera;
-
+	private bool _canSelectFan = false;
+	private bool _detectedCamera = false;
+	private bool _detectedLight = false;
+	private bool _smokeCamera = false;
+	private bool _smokeOn = false;
+	private float _cameraIncrease = 10.0f;
+	private float _detection = 0f;
+	private float _detectionDecrease = 1.0f;
+	private float _detectionIncrease = 5.0f;
+	private float _detectionTotal = 300f;
 	private float _moveFB;
 	private float _moveLR;
 	private float _rotX;
 	private float _rotY;
+	public float sensitivity = 5.0f;
+	public float speed = 5.0f;
+	public GameObject camera;
+	public GameObject smoke;
+	public UnityEngine.UI.Image detectionBar;
 
 
 	// Use this for initialization
@@ -34,33 +40,71 @@ public class FPSController : MonoBehaviour
 		_moveLR = Input.GetAxis("Horizontal") * speed;
 
 		_rotX = Input.GetAxis("Mouse X") * sensitivity;
-		_rotY = Input.GetAxis("Mouse Y") * sensitivity;
+		// _rotY = Input.GetAxis("Mouse Y") * sensitivity;
 
 		Vector3 movement = new Vector3(_moveLR, 0, _moveFB);
 		transform.Rotate(0, _rotX, 0);
 		// if (camera.transform.rotation.x > -0.45f && camera.transform.rotation.x < 0.45f)
 		// 	camera.transform.Rotate(-_rotY, 0, 0);
-		Debug.Log("camera.transform.rotation.x: " + camera.transform.rotation.x);
 		movement = transform.rotation * movement;
 		player.Move(movement * Time.deltaTime);
 
 		if (_detectedLight && _detection < _detectionTotal)
-			_detection += 5f;
+			_detection += _detectionIncrease;
+		if (_detectedCamera && _detection < _detectionTotal)
+			_detection += _cameraIncrease;
+		if (!_smokeOn && _smokeCamera && _detection < _detectionTotal)
+			_detection += _cameraIncrease;
 		if (!_detectedLight && _detection > 0)
-			_detection -= 5f;
+			_detection -= _detectionDecrease;
 		detectionBar.fillAmount = _detection / _detectionTotal;
+		if (detectionBar.fillAmount >= 0.75)
+			detectionBar.color = new Color32(255, 0, 0, 255);
+		else
+			detectionBar.color = new Color32(255, 255, 255, 255);
+		if (detectionBar.fillAmount >= 1)
+			UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+
+		if (_canSelectFan && Input.GetKeyDown(KeyCode.E))
+		{
+			smoke.SetActive(true);
+			_smokeOn = true;
+		}
 
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		Debug.Log("Trigger: other.gameObject.tag: " + other.gameObject.tag);
-		_detectedLight = true;
+		if (other.gameObject.tag == "Light")
+			_detectedLight = true;
+		if (other.gameObject.tag == "Camera")
+			_detectedCamera = true;
+		if (other.gameObject.tag == "SmokeCamera")
+			_smokeCamera = true;
+		if (other.gameObject.tag == "Fan")
+			_canSelectFan = true;
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
 		Debug.Log("Trigger: other.gameObject.tag: " + other.gameObject.tag);
-		_detectedLight = false;
+		if (other.gameObject.tag == "Light")
+			_detectedLight = false;
+		if (other.gameObject.tag == "Camera")
+			_detectedCamera = false;
+		if (other.gameObject.tag == "Fan")
+			_canSelectFan = false;
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("Collision: other.gameObject.tag: " + other.gameObject.tag);
+
+	}
+
+	private void OnCollisionExit(Collision other)
+	{
+		Debug.Log("Collision: other.gameObject.tag: " + other.gameObject.tag);
 	}
 }
