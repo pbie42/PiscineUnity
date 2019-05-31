b899fb80fd0792cdc90565801918fd24f16ae24d
Paul BIE <pbie@e1r7p6.42.fr>
2019-05-31 19:49:35 +0200

    [Fix] - Better character movement

--- d08/Assets/Scripts/Maya.cs
+++ d08/Assets/Scripts/Maya.cs
@@ -7,50 +7,93 @@ public class Maya : MonoBehaviour
 	private Animator _animator;
 	private UnityEngine.AI.NavMeshAgent _navMeshAgent;
 	private bool _running;
+	private bool _attack = false;
 	private int _health = 100;
 	private bool _dead = false;
+	Vector3 pos;
+
+	public float speed;
+	public CharacterController controller;
 
 	// Use this for initialization
 	void Start()
 	{
 		_animator = GetComponent<Animator>();
 		_navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		_navMeshAgent.destination = transform.position;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		RaycastHit hit;
-		if (!_dead)
-		{
-			if (Input.GetMouseButton(0))
-			{
-				if (Physics.Raycast(ray, out hit, 100))
-				{
-					Debug.Log("hit: " + hit);
-					_navMeshAgent.destination = hit.point;
-				}
-			}
-			if (Input.GetMouseButtonDown(1))
-			{
-				_dead = true;
-				_animator.SetBool("Running", _running);
-				_animator.SetBool("Dead", _dead);
-			}
-
-			if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
-			{
-				_running = false;
-			}
-			else
-			{
-				_running = true;
-			}
-		}
-
-		if (!_dead)
-			_animator.SetBool("Running", _running);
+		if (Input.GetMouseButton(0))
+			LocatePosition();
+		MoveToPosition();
 	}
+
+	private void LocatePosition()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 1000))
+		{
+			Debug.Log("hit: " + hit);
+			pos = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			_navMeshAgent.destination = hit.point;
+			// _attack = false;
+			_running = true;
+		}
+	}
+
+	private void MoveToPosition()
+	{
+		if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+		{
+			_running = false;
+			_navMeshAgent.velocity = Vector3.zero;
+			_navMeshAgent.destination = transform.position;
+		}
+		else
+		{
+			Quaternion newRotation = Quaternion.LookRotation(pos - transform.position, Vector3.forward);
+			newRotation.x = 0f;
+			newRotation.z = 0f;
+			transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+			_running = true;
+			Debug.Log("_navMeshAgent.remainingDistance: " + _navMeshAgent.remainingDistance);
+		}
+		_animator.SetBool("Running", _running);
+		// _animator.SetBool("Attack", _attack);
+		// _animator.SetBool("Dead", _dead);
+	}
+
 }
+
+// if (!_dead)
+// {
+// 	if (Input.GetMouseButton(0))
+// 	{
+// 		if (Physics.Raycast(ray, out hit, 100))
+// 		{
+// 			Debug.Log("hit: " + hit);
+// 			_navMeshAgent.destination = hit.point;
+// 			// _attack = false;
+// 			_running = true;
+// 		}
+// 	}
+// 	if (Input.GetMouseButtonDown(1))
+// 	{
+// 		// _dead = true;
+// 		_attack = true;
+// 		// _running = false;
+// 		// _navMeshAgent.ResetPath();
+// 		// _navMeshAgent.destination = transform.position;
+// 		// _animator.SetBool("Running", _running);
+// 		// _animator.SetBool("Dead", _dead);
+// 		// _animator.SetBool("Attack", _attack);
+// 		// _navMeshAgent.isStopped = true; // keeps sliding
+// 	}
+
+
+// }
