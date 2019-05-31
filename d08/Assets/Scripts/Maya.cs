a8c7a07b18c01e7906a4f909678cfbe3febe7f73
Paul BIE <pbie@e1r7p6.42.fr>
2019-05-31 18:05:43 +0200

    [Add] - Player and Camera movement

--- d08/Assets/Scripts/Maya.cs
+++ d08/Assets/Scripts/Maya.cs
@@ -7,6 +7,8 @@ public class Maya : MonoBehaviour
 	private Animator _animator;
 	private UnityEngine.AI.NavMeshAgent _navMeshAgent;
 	private bool _running;
+	private int _health = 100;
+	private bool _dead = false;
 
 	// Use this for initialization
 	void Start()
@@ -21,24 +23,34 @@ public class Maya : MonoBehaviour
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
 		RaycastHit hit;
-
-		if (Input.GetMouseButtonDown(0))
+		if (!_dead)
 		{
-			if (Physics.Raycast(ray, out hit, 100))
+			if (Input.GetMouseButton(0))
 			{
-				_navMeshAgent.destination = hit.point;
+				if (Physics.Raycast(ray, out hit, 100))
+				{
+					Debug.Log("hit: " + hit);
+					_navMeshAgent.destination = hit.point;
+				}
+			}
+			if (Input.GetMouseButtonDown(1))
+			{
+				_dead = true;
+				_animator.SetBool("Running", _running);
+				_animator.SetBool("Dead", _dead);
+			}
+
+			if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+			{
+				_running = false;
+			}
+			else
+			{
+				_running = true;
 			}
 		}
 
-		if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
-		{
-			_running = false;
-		}
-		else
-		{
-			_running = true;
-		}
-
-		_animator.SetBool("Running", _running);
+		if (!_dead)
+			_animator.SetBool("Running", _running);
 	}
 }
---
+++ d08/Assets/Scripts/MayaCam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayaCam : MonoBehaviour
+{
+
+	public Transform target;
+	public float distance = 5f;
+	public float minDistance = 1f;
+	public float maxDistance = 7f;
+	public Vector3 offset;
+	public float smoothSpeed = 5f;
+	public float scrollSensitivity = 1f;
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		// transform.position = new Vector3(target.position.x + 4, target.position.y + 10, target.position.z + 9);
+		if (!target)
+		{
+			Debug.Log("No Target");
+			return;
+		}
+
+		float num = Input.GetAxis("Mouse ScrollWheel");
+		distance -= num * scrollSensitivity;
+		distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+		Vector3 pos = target.position + offset;
+		pos -= transform.forward * distance;
+
+		transform.position = Vector3.Lerp(transform.position, pos, smoothSpeed * Time.deltaTime);
+	}
+}
