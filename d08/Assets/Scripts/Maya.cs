55308270cdafdf3ff9b32ab747ef9e8227df6e32
Paul BIE <pbie@e1r7p6.42.fr>
2019-05-31 16:53:49 +0200

    [Add] - beginnings

---
+++ d08/Assets/Scripts/Maya.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maya : MonoBehaviour
+{
+	private Animator _animator;
+	private UnityEngine.AI.NavMeshAgent _navMeshAgent;
+	private bool _running;
+
+	// Use this for initialization
+	void Start()
+	{
+		_animator = GetComponent<Animator>();
+		_navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if (Physics.Raycast(ray, out hit, 100))
+			{
+				_navMeshAgent.destination = hit.point;
+			}
+		}
+
+		if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+		{
+			_running = false;
+		}
+		else
+		{
+			_running = true;
+		}
+
+		_animator.SetBool("Running", _running);
+	}
+}
